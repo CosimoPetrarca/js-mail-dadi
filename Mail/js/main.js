0414adad6883c5d1ca45fedf44a5d0ab4ce914d2
P4anix <pancok_1@yahoo.it>
2023-02-24 14:17:07 +0100

    mail done

--- Mail/js/main.js
+++ Mail/js/main.js
@@ -1,1 +1,29 @@
 'use strict'
+
+/* Chiedi all'utente di inserire la sua e-mail */
+const mailUtente = prompt("Inserisci la tua e-mail:");
+
+/* Definisci l'array di mail consentite */
+const mailConsentite = ["birra@yahoo.com", "pizza@gmail.com", "vino@hotmail.com", "alcool@libero.it"];
+
+/* Inizializza una variabile per tenere traccia se l'e-mail dell'utente è presente nell'array di e-mail consentite */
+const emailTrovata = false;
+
+/* Uso il ciclo "for" per cercare l'e-mail dell'utente */
+for (let i = 0; i < mailConsentite.length; i++) {
+  if (mailConsentite[i] === mailUtente) {
+    emailTrovata = true;
+    break;
+  }
+}
+
+/* Stampo i messaggi in base a se la mail è stata trovata o meno */
+if (emailTrovata) {
+  /* Messaggio di benvenuto se l'utente è autorizzato */
+  console.log("Benvenuto, " + mailUtente + "!");
+  alert("Benvenuto, " + mailUtente + "!");
+} else {
+  /* Messaggio se l'utente è non è autorizzato */
+  console.log("Mi dispiace, " + mailUtente + ". Sei stato bannato in precedenza.");
+  alert("Mi dispiace, " + mailUtente + ". Sei stato bannato in precedenza.");
+}
